e93d09b6646138e13ce5915ae67238455fdfb8aa
al.agaev <aagaev@roox.ru>
2025-07-16 21:35:02 +0300

    fix jdbc tests

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -8,11 +9,15 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.util.ListUserExtractor;
 import ru.javawebinar.topjava.util.UserExtractor;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -46,12 +51,18 @@ public class JdbcUserRepository implements UserRepository {
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
-        } else if (namedParameterJdbcTemplate.update("""
-                   UPDATE users SET name=:name, email=:email, password=:password, 
-                   registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id
-                """, parameterSource) == 0) {
-            return null;
+            insertRoles(user);
+        } else {
+// тут совсем не уверен, как будто зачищать все записи и обновлять их это странное решение,
+// но с другой стороны пытаться сравнить какая именно роль изменилась как будто оверхед
+            deleteRoles(user);
+            insertRoles(user);
+            namedParameterJdbcTemplate.update("""
+                       UPDATE users SET name=:name, email=:email, password=:password, 
+                       registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id
+                    """, parameterSource);
         }
+
         return user;
     }
 
@@ -80,5 +91,29 @@ public class JdbcUserRepository implements UserRepository {
     public List<User> getAll() {
         List<User> query = jdbcTemplate.query("SELECT * FROM users u LEFT JOIN user_role ur ON u.id = ur.user_id ORDER BY name, email", new ListUserExtractor());
         return query;
+    }
+
+    private void insertRoles(User u) {
+        List<Role> roles = new ArrayList<>(u.getRoles());
+
+        jdbcTemplate.batchUpdate(
+                "INSERT INTO user_role (user_id, role) VALUES (?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setInt(1, u.getId());
+                        ps.setString(2, roles.get(i).name());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return roles.size();
+                    }
+                }
+        );
+    }
+
+    private void deleteRoles(User u) {
+        jdbcTemplate.update("DELETE FROM user_role WHERE user_id=?", u.getId());
     }
 }
